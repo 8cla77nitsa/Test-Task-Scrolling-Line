87da49fe29da84eedc0071750c27a79b7344587a
Artem Vorobev <8cla77nitsa@gmail.com>
2024-07-24 16:09:06 +0300

    Reworked text thing

--- main.py
+++ main.py
@@ -2,28 +2,17 @@ from manim import *
 
 config.pixel_width = 100
 config.pixel_height = 100
-config.frame_width = 3
+config.frame_width = 1
 
 
 class ScrollingLine(Scene):
     def construct(self):
         global mytext
-        mytext = mytext.upper().split()
-        textgroup = VGroup()
-        words = []
-
-        for i in range(len(mytext)):
-            if i == 0:
-                words.append(Text(mytext[i], font="Arial", font_size=144))
-            else:
-                word = Text(mytext[i], font="Arial", font_size=144)
-                word.next_to(words[i - 1], RIGHT, buff=0.7)
-                words.append(word)
-            textgroup.add(words[i])
-
-        textgroup.to_edge(RIGHT).shift(RIGHT * textgroup.width + RIGHT * 0.5)
+        mytext.replace(" ", f'<span fgcolor="{BLACK}">_</span>')
+        textobject = MarkupText(mytext, font="Arial", font_size=48)
+        textobject.to_edge(RIGHT).shift(RIGHT * textobject.width + RIGHT)
         self.play(
-            textgroup.animate.to_edge(LEFT).shift(LEFT * textgroup.width + LEFT * 0.5),
+            textobject.animate.to_edge(LEFT).shift(LEFT * textobject.width + LEFT),
             run_time=3, rate_func=linear)
 
 
